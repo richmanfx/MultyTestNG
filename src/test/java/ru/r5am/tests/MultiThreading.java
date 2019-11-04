7d88234f84f3591590ffaa940301d967aec9f41d
richmanfx <richmanfx@yandex.ru>
2019-11-04 12:27:51 +0300

    Три тестовых метода, выполняющихся параллельно.

--- src/test/java/ru/r5am/tests/MultiThreading.java
+++ src/test/java/ru/r5am/tests/MultiThreading.java
@@ -1,7 +1,42 @@
 package ru.r5am.tests;
 
+import org.testng.annotations.Test;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class MultiThreading {
 
+    private static final Logger log = LogManager.getLogger();
 
+    @Test
+    public void firstMethod() throws InterruptedException {
+        log.info("Firs method");
+        longJob(1);
+    }
+
+    @Test
+    public void secondMethod() throws InterruptedException {
+        log.info("Second method");
+        longJob(2);
+    }
+
+    @Test
+    public void thirdMethod() throws InterruptedException {
+        log.info("Third method");
+        longJob(3);
+    }
+
+    /**
+     * Долго выполняющаяся задача
+     * @param jobNumber Номер задачи
+     */
+    private void longJob(int jobNumber) throws InterruptedException {
+
+        for(int i = 0; i < 10; i++) {
+            Thread.sleep(300);
+            log.info("{}", jobNumber);
+        }
+
+    }
 
 }
